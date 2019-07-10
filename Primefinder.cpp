ce8a4002eeb1126f1aa8dfbea0790357d739e4d3
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-07-10 21:42:28 +0200

    Update Primefinder.cpp

--- Primefinder.cpp
+++ Primefinder.cpp
@@ -2,21 +2,20 @@
 #include <vector>
 #include <cmath>
 using namespace std;
-bool prime = true;
 int y = 1;
 std::vector<int> primelist = {};
 int main()
 {
-	std::vector<int> primelist = { 3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101 };
-	primelist.push_back(2);
-	for (int y = 103; y <= 1000; y += 2) {
-		int prime = true;
-		for (int x = 0; x < sizeof(primelist) / 4; x++) {
-			int z = primelist[x];
-			if (y % z == 0) { prime = false; break;}
-			if (z > sqrt(y)) {break;}
-		}
-		if (prime == true) {primelist.push_back(y);}
-	}
-	for (int n : primelist) {std::cout << n << endl;}
-}
+    std::vector<int> primelist = { 3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101 };
+    primelist.push_back(2);
+    for (int y = 103; y <= 1000; y += 2) {
+        bool prime = true;
+        for (int x = 0; x < sizeof(primelist) / 4; x++) {
+            int z = primelist[x];
+			if (y % z == 0) { prime = false; break; };
+			if (z > sqrt(y)) { break; };
+        }
+		if (prime == true) { primelist.push_back(y); };
+    }
+    for (int n : primelist) {std::cout << n << endl;}
+    }
