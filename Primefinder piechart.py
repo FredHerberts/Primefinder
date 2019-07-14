9935958b7b924009ed186d4034b9cecc83394da2
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-07-14 22:28:02 +0200

    Update Primefinder piechart.py

--- Primefinder piechart.py
+++ Primefinder piechart.py
@@ -1,14 +1,8 @@
 import math
-import time
 import matplotlib.pyplot as plt
-
 categories = ["Primes", "Composites"]
 primelist2 = [3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101]
-
-
 number = int(input("enter the number"))
-
-start = time.time()
 
 def primefinder(number):
     for y in range(103, number + 1,2):
@@ -26,7 +20,6 @@ def primefinder(number):
 primefinder(int(number))
 a = len(primelist2)+1
 b = number - a
-categories = ["Primes", "Composites"]
 sizes =[a,b]
 
 fig1, ax1 = plt.subplots()
