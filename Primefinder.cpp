c40376b0cf9f166a947ba274fec75f2a27e9973a
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-07-15 08:41:02 +0200

    Update Primefinder.cpp

--- Primefinder.cpp
+++ Primefinder.cpp
@@ -12,7 +12,6 @@ int main()
 {
     std::vector<int> primelist = { 3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101 };
     cout << 2 << endl;
-    for (int n : primelist) { std::cout << n << endl; }
     for (int y = 103; y <= number; y += 2) {
         bool prime = true;
         for (int x = 0; x < number; x++) {
@@ -22,7 +21,7 @@ int main()
         }
         if (prime == true) { primelist.push_back(y);};
     }
-    cout << primelist.back() << endl;
+    for (int n : primelist) { std::cout << n << endl; }
     int stop_s=clock();
     cout<<(stop_s-start_s)/double(CLOCKS_PER_SEC);
 }
