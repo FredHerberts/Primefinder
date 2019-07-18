532d6c5767e51cfaf73583bb362004c3662ea49d
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-07-18 11:35:27 +0200

    Update Primefinder v2.py

--- Primefinder v2.py
+++ Primefinder v2.py
@@ -5,19 +5,15 @@ primelist= [2]
 
 def primefinder(number):
     prime = [True for x in range(number + 1)]
-    y = 3
-    while y < math.sqrt(number+1):
+    for y in range(3,int(math.sqrt(number+1)),2):
         if prime[y] == True:
             for x in range(y+y, number + 1,y):
                 prime[x] = False
-            y +=2
-        else:
-            y +=2
     for x in range(3,number+1,2):
         if prime[x] == True:
             primelist.append(x)
 
-primefinder(1000000)
+primefinder(10000)
 print(primelist)
 
 end = time.time()
