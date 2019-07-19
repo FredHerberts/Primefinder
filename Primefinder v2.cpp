10c9810543314a217c4112a7cff6cdb8be33034c
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-07-19 09:53:25 +0200

    Create Primefinder v2.cpp

---
+++ Primefinder v2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h> 
+#include <vector>
+#include<ctime>
+using namespace std;
+std::vector<int> primelist = {2};
+int number = 1000000;
+int start_s=clock() ;
+int main()
+{
+    int prime[number+1] ={};
+    std::fill_n(prime, number+1, 1);
+    for (int y = 3;y < sqrt(number+1);y+= 2)
+    {if (prime[y] == 1)
+    {for (int x = y+y;x < number+1; x += y)
+    {prime[x] = 0;}}}
+    for ( int x=3; x < number+1; x+=2){
+      if (prime[x] == true){primelist.push_back(x);}}
+     for(int i=0; i < primelist.size(); i++)
+      std::cout << primelist.at(i) << ' ';
+      int stop_s = clock();
+    cout << (stop_s - start_s) / double(CLOCKS_PER_SEC);
+	return 0;
+      
+      }
+
